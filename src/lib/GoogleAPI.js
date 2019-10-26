4c5f619c6b8e4b17ce44cc667606f598e14c62ea
Mario A. Santini <alexmario74@gmail.com>
2019-10-26 17:21:25 +0200

    added google API wrap

---
+++ src/lib/GoogleAPI.js
@@ -0,0 +1,25 @@
+const GOOGLE_API_ENDPOINT = "https://www.googleapis.com/books/v1/volumes";
+
+async function search(term) {
+    const query = term.trim();
+
+    return fetch(buildURL(query))
+        .then(response => response.json())
+        .then(toBookList)
+        .catch(e => console.error("Google API error:", e));
+}
+
+function buildURL(q) {
+    return GOOGLE_API_ENDPOINT + "?q=" + encodeURIComponent(q);
+}
+
+function toBookList({ items }) {
+    return items
+        .map(({ volumeInfo }) => ({
+            title: volumeInfo.title,
+            author: volumeInfo.authors.join(", "),
+            description: volumeInfo.description 
+        }));
+}
+
+export default search;
